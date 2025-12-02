67529b8a2805c41fd2883acf5ed32b76cea7c67b
harsh6468 <harsh.bhumca21@gmail.com>
2025-12-03 00:59:33 +0530

    Improved config

--- backend/src/configs/config.py
+++ backend/src/configs/config.py
@@ -12,7 +12,7 @@ class Config(LoggerFactory):
         self.logger = self.get_logger("Config")
         self.host: str = os.getenv("HOST", "0.0.0.0")
         self.port: int = int(os.getenv("PORT", "5000"))
-        self.reload: bool = os.getenv("RELOAD", "True") == "True"
+        self.reload: bool = os.getenv("RELOAD", "False") == "True"
         self.allowed_origins: str = os.getenv("ALLOWED_ORIGINS", "*")
         self.groq_api_key: str = os.getenv("GROQ_API_KEY", None)
         self.groq_model: str = os.getenv("GROQ_MODEL", "openai/gpt-oss-20b")
